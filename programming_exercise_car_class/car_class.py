ff8f7c12ca3841a8f125b50d1e88092790601818
unknown <achillesaeon8-svg>
2026-06-05 00:52:08 +0800

    Adding Class Variable and Loop Structure for Speed

--- programming_exercise_car_class/car_class.py
+++ programming_exercise_car_class/car_class.py
@@ -1,12 +1,18 @@
 class Car:
+
     def __init__(self):
         self.__year_model = year_model
         self.__make = car_brand
         self.__speed = 0
 
+    cool_car = ('Mclaren', '2017')
+
     def speed(self):
         return self.__speed
     
-    year_model = '2017'
-
-    car_brand = 'Mclaren'
+    def acceleration(self):
+        print('Accelerating: ')
+        for acceleration in range(5):
+            self.cool_car.accelerate()
+            current_speed = self.cool_car.get_speed()
+            print(current_speed)
