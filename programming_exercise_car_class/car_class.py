733f74b2163da0f16473a39896b4028b81eb64d4
unknown <achillesaeon8-svg>
2026-06-05 03:00:42 +0800

    Adding Output Designs, Refactoring Class Variable Names, and Fixing Indention Aligning Within the Output Designs.

--- programming_exercise_car_class/car_class.py
+++ programming_exercise_car_class/car_class.py
@@ -9,16 +9,20 @@ class Car:
         return self.__speed
     
     def acceleration(self):
-        print('Accelerating: ')
+        print('Accelerating: \n')
         for count in range(5):
             self.__speed += 5
             print(f'Current Speed: {self.speed()} km/h.')
 
+        print('=' * 60)
+
     def brake(self):
-        print('Braking: ')
+        print('Braking: \n')
         for count in range(5):
             if self.__speed >= 5:
                 self.__speed -= 5
             else: 
                 self.__speed = 0
             print(f'Current Speed: {self.speed()} km/h.')
+
+        print('=' * 60)
--- programming_exercise_car_class/main.py
+++ programming_exercise_car_class/main.py
@@ -2,11 +2,15 @@ from car_class import Car
 
 class CarDyanmics:
     def __init__(self):
-        cool_car_data = ('Mclaren', '2017')
+        cool_car_data = ('Mclaren 720s', '2017')
         car_results = Car(cool_car_data[1], cool_car_data[0])
 
-        print(f'Car Created: {cool_car_data[1]} ({cool_car_data[0]})')
-        print(f'Starting Speed: {car_results.speed()} km/h.')
+        print('=' * 60)
+        print(f'             Car Year & Model: {cool_car_data[1]} {cool_car_data[0]}')
+        print('=' * 60)
+
+        print(f'Starting Speed: {car_results.speed()} km/h.\n')
+
 
         car_results.acceleration()
         car_results.brake()
