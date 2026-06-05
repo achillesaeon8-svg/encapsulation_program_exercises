37a2c581b829d865103cd0dd47dbf6b33055fa14
unknown <achillesaeon8-svg>
2026-06-05 23:54:33 +0800

    Adding Output Designs and Suitable Indentions for Outputs Titles.

--- programming_exercise_pet_class/pet_main.py
+++ programming_exercise_pet_class/pet_main.py
@@ -4,7 +4,9 @@ class PetInfoOrganizer:
 
     input_pet = Pet()
 
-    print('PET INFORMATION REGISTRATION')
+    print('=' * 80)
+    print('                         PET INFORMATION REGISTRATION')
+    print('=' * 80)
 
     input_name = input('Enter you pet name: ')
     input_animal_type = input('What type of animal is your pet?: ')
@@ -14,10 +16,14 @@ class PetInfoOrganizer:
     input_pet.set_animal_type(input_animal_type)
     input_pet.set_age(input_age)
 
-    print('COLLECTED PET DATA')
+    print('=' * 80)
+    print('                              COLLECTED PET DATA')
+    print('=' * 80)
+
     print(f'Pet name: {input_pet.get_name()}.')
     print(f'Animal Type: {input_pet.get_animal_type()}.')
     print(f'Pet Age: {input_pet.get_age()}.')
+    print('=' * 80)
 
 if __name__ == '__main__':
     PetInfoOrganizer()
