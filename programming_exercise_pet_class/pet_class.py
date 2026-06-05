77fa5c1fd3a0b5c392f4652c191373da4d9c6e73
unknown <achillesaeon8-svg>
2026-06-05 23:08:48 +0800

    Application of Default Constructor and Mutator Methods.

--- programming_exercise_pet_class/pet_class.py
+++ programming_exercise_pet_class/pet_class.py
@@ -1,6 +1,15 @@
 class Pet:
     
-    def __init__(self, pet_name, animal_type, pet_age):
+    def __init__(self):
+        self.__name = ' '
+        self.__animal_type = ' '
+        self.__age = 0
+
+    def set_name(self, pet_name):
         self.__name = pet_name
+
+    def set_animal_type(self, animal_type):
         self.__animal_type = animal_type
+
+    def set_age(self, pet_age):
         self.__age = pet_age
