547220fadee195d34872c18bad183d89f4756f49
unknown <achillesaeon8-svg>
2026-06-06 02:08:01 +0800

    Refactoring Class Variables and Adding Spaces, Indentions, and Designs.

--- programming_exercise_fan_class/test_fan.py
+++ programming_exercise_fan_class/test_fan.py
@@ -6,7 +6,7 @@ class FanSpecifications:
 
     first_fan.set_speed(Fan.fast)
     first_fan.set_radius(10)
-    first_fan.set_color('yellow')
+    first_fan.set_color('Yellow')
     first_fan.set_on(on=True)
 
     second_fan = Fan()
@@ -15,18 +15,25 @@ class FanSpecifications:
     second_fan.set_radius(5)
     second_fan.set_color(color='Blue')
     second_fan.set_on(on=False)
-
-    print('FAN 1 SPECIFICATIONS')
+    print('=' *50)
+    print('=' *50)
+    print('                 FAN 1 SPECIFICATIONS')
+    print('=' *50)
+    
     print(f'Speed: {first_fan.get_speed()}')
     print(f'Radius: {first_fan.get_radius()}')
     print(f'Color: {first_fan.get_color()}')
     print(f'Status Mode: {first_fan.get_on()}\n')
 
-    print('FAN 2 SPECIFICATIONS')
+    print('=' *50)
+    print('                 FAN 2 SPECIFICATIONS')
+    print('=' *50)
+
     print(f'Speed: {second_fan.get_speed()}')
     print(f'Radius: {second_fan.get_radius()}')
     print(f'Color: {second_fan.get_color()}')
     print(f'Status Mode: {second_fan.get_on()}\n')
+    print('=' *50)
 
 if __name__ == '__main__':
     FanSpecifications()
