7853fdb2a1640cdd969556bbe57bff0e7950d11c
unknown <achillesaeon8-svg>
2026-06-06 01:49:55 +0800

    Application of Fan 1 Display Output.

--- programming_exercise_fan_class/test_fan.py
+++ programming_exercise_fan_class/test_fan.py
@@ -1,4 +1,23 @@
 from fan_class import Fan
 
 class FirstFan:
-    pass
+    
+    first_fan = Fan()
+
+    first_fan.set_speed(Fan.fast)
+    first_fan.set_radius(10)
+    first_fan.set_color('yellow')
+    first_fan.set_color(on=True)
+
+    second_fan = Fan()
+
+    second_fan.set_speed(Fan.medium)
+    second_fan.set_radius(5)
+    second_fan.set_color(color='Blue')
+    second_fan.set_on(on=False)
+
+    print('FAN 1 SPECIFICATIONS')
+    print(f'Speed: {first_fan.get_speed()}.')
+    print(f'Radius: {first_fan.get_radius()}.')
+    print(f'Color: {first_fan.get_color()}.')
+    print(f'Status Mode: {first_fan.get_on()}.\n')
