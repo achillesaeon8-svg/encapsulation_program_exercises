9891a245fe0596e2f0cd51b8bb80a591bdda9e90
unknown <achillesaeon8-svg>
2026-06-05 23:34:55 +0800

    Application of Default Constructor, Implementation of Interactive Console Input, Data Output Draft Creation, and Data Encapsulation.

--- programming_exercise_pet_class/pet_main.py
+++ programming_exercise_pet_class/pet_main.py
@@ -1,4 +1,23 @@
 from pet_class import Pet
 
 class PetInfoOrganizer:
-    pass
+
+    input_pet = Pet()
+
+    print('PET INFORMATION REGISTRATION')
+
+    input_name = input('Enter you pet name: ')
+    input_animal_type = input('What type of animal is your pet?: ')
+    input_age = input('How old is your pet?: ')
+
+    input_pet.set_name(input_name)
+    input_pet.set_animal_type(input_animal_type)
+    input_pet.set_age(input_age)
+
+    print('COLLECTED PET DATA')
+    print(f'Pet name: {input_pet.get_name()}.')
+    print(f'Animal Type: {input_pet.get_animal_type()}.')
+    print(f'Pet Age: {input_pet.get_age()}.')
+
+if __name__ == '__main__':
+    PetInfoOrganizer()
