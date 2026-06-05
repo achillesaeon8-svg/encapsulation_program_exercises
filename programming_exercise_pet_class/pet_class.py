274c3db29d1c67e7be7bba0dc134e24c8e413302
unknown <achillesaeon8-svg>
2026-06-05 21:33:54 +0800

    Adding Data Attributes for Pet Class File.

--- programming_exercise_pet_class/pet_class.py
+++ programming_exercise_pet_class/pet_class.py
@@ -1,2 +1,6 @@
 class Pet:
-    pass
+    
+    def __init__(self, pet_name, animal_type, pet_age):
+        self.__name = pet_name
+        self.__animal_type = animal_type
+        self.__age = pet_age
