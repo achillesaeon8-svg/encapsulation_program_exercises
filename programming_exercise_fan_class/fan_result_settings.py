258fc58d820807dcd67d88a17e33043aea56ee98
unknown <achillesaeon8-svg>
2026-06-06 22:47:22 +0800

    Application of Additional Feature of Text Colors Based on their own text.

--- programming_exercise_fan_class/fan_result_settings.py
+++ programming_exercise_fan_class/fan_result_settings.py
@@ -6,14 +6,24 @@ class FanOutcome(Fan):
         super().__init__(speed, radius, color, on)
 
     def fan_display_output(self, fan_number):
+
+        colors = { 
+            
+            'Yellow': '\033[93m',
+            'Blue': '\033[94m',
+            'Normal': '\033[0m'
+        }
         
+        color_code = colors.get(self.get_color(), '\033[97m')
+        reset_color = colors['Normal']
+
         print('=' *50)       
         print(f'                 {fan_number} SPECIFICATIONS')
         print('=' *50)
         
         print(f'Speed: {self.get_speed()}')
         print(f'Radius: {self.get_radius()}')
-        print(f'Color: {self.get_color()}')
+        print(f'Color: {color_code}{self.get_color()}{reset_color}')
         print(f'Status Mode: {self.get_on()}')
 
         print('=' *50)
