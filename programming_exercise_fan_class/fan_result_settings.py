dfe40cdfad9fd5af8f30aca3c34f8626db7074fc
unknown <achillesaeon8-svg>
2026-06-06 22:08:57 +0800

    Application of Constructor and Instance Variable.

--- programming_exercise_fan_class/fan_result_settings.py
+++ programming_exercise_fan_class/fan_result_settings.py
@@ -1,2 +1,6 @@
-class FanOutcome:
-    pass
+from fan_class import Fan
+
+class FanOutcome(Fan):
+    
+    def __init__(self, speed=Fan.slow, radius=5, color='Blue', on=False):
+        super().__init__(speed, radius, color, on)
