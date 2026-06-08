29e7ef9bec699011b1f27732c3940e625603b7fa
unknown <achillesaeon8-svg>
2026-06-08 23:00:41 +0800

    Transfer of Display Method to Pet Display Mechanics File.

--- programming_exercise_pet_class/pet_display_mechanics.py
+++ programming_exercise_pet_class/pet_display_mechanics.py
@@ -4,3 +4,13 @@ class PetOutcome(Pet):
     
     def __init__(self):
         super().__init__()
+
+    def pet_display_output(self):
+        print('=' * 80)
+        print('                              COLLECTED PET DATA')
+        print('=' * 80)
+
+        print(f'Pet name: {input_pet.get_name()}.')
+        print(f'Animal Type: {input_pet.get_animal_type()}.')
+        print(f'Pet Age: {input_pet.get_age()}.')
+        print('=' * 80)
